3acbe9651b4adf5994dcf7ab30dc03d915e78e77
zhao yitao <johocn@163.com>
2026-07-28 19:51:23 +0800

    feat(inventory-plugin): add StockInOrder + StockInOrderLine entities

---
+++ packages/inventory-plugin/src/entities/stock-in-order.entity.ts
@@ -0,0 +1,45 @@
+// e:\code\vendure\packages\inventory-plugin\src\entities\stock-in-order.entity.ts
+import { Column, Entity, ManyToMany, ManyToOne, OneToMany, JoinTable } from 'typeorm';
+import { Channel, DeepPartial, ID, StockLocation, VendureEntity } from '@vendure/core';
+
+import { StockInState } from '../constants';
+
+@Entity()
+export class StockInOrder extends VendureEntity {
+    constructor(input?: DeepPartial<StockInOrder>) {
+        super(input);
+    }
+
+    @Column() code: string;
+    @Column({ default: 'Pending' }) state: StockInState;
+    @Column({ nullable: true }) type: string;
+    @Column({ nullable: true }) note: string;
+    @Column({ nullable: true }) staffId: string;
+
+    @ManyToOne(() => StockLocation)
+    targetLocation: StockLocation;
+    @Column() targetLocationId: ID;
+
+    @OneToMany(() => StockInOrderLine, line => line.order)
+    lines: StockInOrderLine[];
+
+    @Column({ type: 'timestamp', nullable: true }) completedAt?: Date;
+    @Column({ type: 'timestamp', nullable: true }) cancelledAt?: Date;
+
+    @ManyToMany(() => Channel)
+    @JoinTable()
+    channels: Channel[];
+}
+
+@Entity()
+export class StockInOrderLine extends VendureEntity {
+    constructor(input?: DeepPartial<StockInOrderLine>) {
+        super(input);
+    }
+
+    @ManyToOne(() => StockInOrder) order: StockInOrder;
+    @Column() orderId: ID;
+    @Column() productVariantId: ID;
+    @Column() quantity: number;
+    @Column({ nullable: true }) unitPrice: number | null;
+}
