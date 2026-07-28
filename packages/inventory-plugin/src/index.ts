43954c98084f4c178c40038094bc0348e2144516
zhao yitao <johocn@163.com>
2026-07-28 19:47:29 +0800

    feat(inventory-plugin): add index.ts barrel exports

---
+++ packages/inventory-plugin/src/index.ts
@@ -0,0 +1,8 @@
+// e:\code\vendure\packages\inventory-plugin\src\index.ts
+export * from './inventory.plugin';
+export * from './constants';
+export * from './inventory.service';
+export * from './entities/stock-in-order.entity';
+export * from './entities/stock-out-order.entity';
+export * from './entities/stock-move-order.entity';
+export * from './entities/stocktake-order.entity';
